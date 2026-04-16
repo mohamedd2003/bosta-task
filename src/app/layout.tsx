87a7a901d10fdad8d3a30f455ef9928702883432
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2026-04-16 14:19:56 +0200

    fix: remove background color from body in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${outfit.variable} bg-yellow-500 font-sans antialiased`}>
+      <body className={`${outfit.variable}  font-sans antialiased`}>
         <Navbar />
         <main className="min-h-[calc(100vh-4rem)]">{children}</main>
         <Footer />
